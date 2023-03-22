e6dfac444b30b655b1e3f6de982913627ad73483
RummahH <jah.hasant@gmail.com>
2023-03-22 23:24:12 +0600

    d

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(
     origin: [
       'http://localhost:3000',
       'https://safetyap.netlify.app',
-      'https://apps-sos.com/',
+      'https://apps-sos.com',
     ],
     credentials: true,
   })
